c133af1da44c0350c3fbc587d26f57b21b2c54df
Tania <drob.tania@gmail.com>
2021-04-24 23:24:12 +0300

    works pretty fine, TODO: styles, rooms?

--- src/app/chatroom-general/chatroom-general.component.ts
+++ src/app/chatroom-general/chatroom-general.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-chatroom-general',
   templateUrl: './chatroom-general.component.html',
   styleUrls: ['./chatroom-general.component.css']
 })
-export class ChatroomGeneralComponent implements OnInit {
-
+export class ChatroomGeneralComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scroller')
+  private feedContainer!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  scrollToBottom(): void {
+    this.feedContainer.nativeElement.scrollTop = 
+      this.feedContainer.nativeElement.scrollHeight;
+  }
+
+  ngAfterViewChecked(){
+    this.scrollToBottom();
+  }
 }
